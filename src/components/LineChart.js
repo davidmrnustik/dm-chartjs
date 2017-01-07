6ad8cb3f92d4da9eb6dc8d334fb1960d5f882567
David Mrnustik <david.mrnustik@hotmail.com>
2017-01-07 22:25:22 +0100

    Add gradients to Line Chart

--- src/components/LineChart.js
+++ src/components/LineChart.js
@@ -6,24 +6,37 @@ export default React.createClass({
     const x = ["01", "02", "03", "04", "05", "06"];
     const y = this.props.chart;
     const ctx = this.refs.chartBar.getContext('2d');
-    const gradient = ctx.createLinearGradient(0, 0, 0, 280);
-    gradient.addColorStop(0, 'rgba(9, 160, 187,0.4)');   
-    gradient.addColorStop(1, 'rgba(9, 160, 187,0)');
+
+    const gradientBlue = ctx.createLinearGradient(0, 0, 0, 100);
+    gradientBlue.addColorStop(0, 'rgba(18, 59, 106,0.5)');   
+    gradientBlue.addColorStop(1, 'rgba(18, 59, 106,0)');
+
+    const gradientTurquoise = ctx.createLinearGradient(0, 0, 0, 200);
+    gradientTurquoise.addColorStop(0, 'rgba(9, 160, 187,0.5)');   
+    gradientTurquoise.addColorStop(1, 'rgba(9, 160, 187,0)');
+
+    const gradientGreen = ctx.createLinearGradient(0, 0, 0, 300);
+    gradientGreen.addColorStop(0, 'rgba(15, 189, 119,0.5)');   
+    gradientGreen.addColorStop(1, 'rgba(15, 189, 119,0)');
+
+    const gradientOrange = ctx.createLinearGradient(0, 0, 0, 300);
+    gradientOrange.addColorStop(0, 'rgba(255,197,20,0.5)');   
+    gradientOrange.addColorStop(1, 'rgba(255,197,20,0)');
+
     const dataChart = {
       labels: x,
       datasets: [
         {
           label: 'Propuesta venta',
-          borderColor: 'rgba(9, 160, 187,1.0)',
+          borderColor: 'rgba(18, 59, 106,1.0)',
           borderWidth: 2,
-          pointBorderColor: 'rgba(9, 160, 187,1.0)',
+          pointBorderColor: 'rgba(18, 59, 106,1.0)',
           pointBackgroundColor: '#ffffff',
           pointBorderWidth: 2,
           pointRadius: 4,
           pointHoverRadius: 5,
-          pointHoverBackgroundColor: 'rgba(9, 160, 187,1.0)',
-          backgroundColor: gradient,
-          pointStyle: 'circle',
+          pointHoverBackgroundColor: 'rgba(18, 59, 106,1.0)',
+          backgroundColor: gradientBlue,
           data: y[0]
         },
         {
@@ -36,42 +49,49 @@ export default React.createClass({
           pointRadius: 4,
           pointHoverRadius: 5,
           pointHoverBackgroundColor: 'rgba(9, 160, 187,1.0)',
-          backgroundColor: gradient,
-          pointStyle: 'triangle',
+          backgroundColor: gradientTurquoise,
           data: y[1]
         },
         {
           label: 'Entrega',
-          borderColor: 'rgba(9, 160, 187,1.0)',
+          borderColor: 'rgba(15, 189, 119,1.0)',
           borderWidth: 2,
-          pointBorderColor: 'rgba(9, 160, 187,1.0)',
+          pointBorderColor: 'rgba(15, 189, 119,1.0)',
           pointBackgroundColor: '#ffffff',
           pointBorderWidth: 2,
           pointRadius: 4,
           pointHoverRadius: 5,
-          pointHoverBackgroundColor: 'rgba(9, 160, 187,1.0)',
-          backgroundColor: gradient,
-          pointStyle: 'star',
+          pointHoverBackgroundColor: 'rgba(15, 189, 119,1.0)',
+          backgroundColor: gradientGreen,
           data: y[2]
         },
         {
           label: 'Campañas',
-          borderColor: 'rgba(9, 160, 187,1.0)',
+          borderColor: 'rgba(255,197,20,1.0)',
           borderWidth: 2,
-          pointBorderColor: 'rgba(9, 160, 187,1.0)',
+          pointBorderColor: 'rgba(255,197,20,1.0)',
           pointBackgroundColor: '#ffffff',
           pointBorderWidth: 2,
           pointRadius: 4,
           pointHoverRadius: 5,
-          pointHoverBackgroundColor: 'rgba(9, 160, 187,1.0)',
-          backgroundColor: gradient,
-          pointStyle: 'rectRot',
+          pointHoverBackgroundColor: 'rgba(255,197,20,1.0)',
+          backgroundColor: gradientOrange,
           data: y[3]
         }
       ]
     };
     const options = {
-
+      legend: {
+        display: true,
+        position: 'bottom',
+        labels: {
+          boxWidth: 14,
+          fontSize: 11,
+          fontStyle: 'bold',
+          padding: 20,
+          usePointStyle: true
+        }
+      },
     }
 
     new Chart(ctx, {
